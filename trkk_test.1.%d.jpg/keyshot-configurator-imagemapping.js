f6f1871a659afa7b93e8fcc6cd0311e59df9f725
Chezz_ <cesarop@hotmail.com>
2021-04-21 08:52:34 -0500

    init

---
+++ trkk_test.1.%d.jpg/keyshot-configurator-imagemapping.js
@@ -0,0 +1,30 @@
+imagemappingdata =
+[
+    {
+        "image": "trkk_test.1.1.jpg",
+        "modelsets": [
+            552585279
+        ],
+        "multimaterials": [
+            "284+289"
+        ]
+    },
+    {
+        "image": "trkk_test.1.2.jpg",
+        "modelsets": [
+            552585279
+        ],
+        "multimaterials": [
+            "284+291"
+        ]
+    },
+    {
+        "image": "trkk_test.1.3.jpg",
+        "modelsets": [
+            552585279
+        ],
+        "multimaterials": [
+            "284+299"
+        ]
+    }
+];
---
+++ trkk_test.1.%d.jpg/keyshot-configurator-modeldata.js
@@ -0,0 +1,44 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "material%20set%201%201.png",
+                    "multimaterials": [
+                        "284+289"
+                    ],
+                    "name": "Material set 1 1"
+                },
+                {
+                    "icon": "material%20set%201%202.png",
+                    "multimaterials": [
+                        "284+291"
+                    ],
+                    "name": "Material set 1 2"
+                },
+                {
+                    "icon": "material%20set%201%203.png",
+                    "multimaterials": [
+                        "284+299"
+                    ],
+                    "name": "Material set 1 3"
+                }
+            ],
+            "name": "Material set 1"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "trekking.png",
+            "materialgroups": [
+                "Material set 1"
+            ],
+            "modelset": 552585279,
+            "name": "Trekking"
+        }
+    ],
+    "studios": []
+};
